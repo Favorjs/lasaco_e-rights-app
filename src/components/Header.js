4ca561bf7e642c51adaf17a19440c51c1515ad2c
Apelng <administrator@apelasset.com>
2026-01-06 01:32:16 +0100

    removed bg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
-import logo2 from '../assets/images/lasaco.jpg';
+import logo2 from '../assets/images/lasaco.png';
 
 const Header = () => {
 
